a590887e6193f1890b7cdeac53086737bc2fc50c
NasirShaik <nasir4urfrnd@gmail.com>
2018-11-19 12:35:11 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  students = [];
+
+  addStd(fn, ln, loc) {
+    this.fs.collection('students').add({ 'fname': fn, 'lname': ln, 'location': loc })
+      .then((x) => console.log(x));
+  }
+
+  delete(id) {
+    this.fs.collection('students').doc(id).delete();
+  }
+
+  constructor(private fs: AngularFirestore) { }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.fs.collection('students').snapshotChanges().subscribe((res) => {
+      this.students = [];
+      res.map((x) => {
+        this.students.push({ 'data': x.payload.doc.data(), 'id': x.payload.doc.id });
+      });
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const config = {
+  apiKey: 'AIzaSyAb4akm_TuRHhxQLPVc25S7h8LEyZqiJJE',
+  authDomain: 'logapp-8514a.firebaseapp.com',
+  databaseURL: 'https://logapp-8514a.firebaseio.com',
+  projectId: 'logapp-8514a',
+  storageBucket: 'logapp-8514a.appspot.com',
+  messagingSenderId: '1058867583912'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
